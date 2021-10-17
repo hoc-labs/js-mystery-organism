625516d75006c02115c2eb0786e6402830f11d4f
Anne Chinn <achinn@nwlink.com>
2021-10-16 17:18:54 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Returns a random DNA base
+const returnRandBase = () => {
+  const dnaBases = ['A', 'T', 'C', 'G'];
+}
+
+// Returns a random single stand of DNA containing 15 bases
+const mockUpStrand = () => {
+}
